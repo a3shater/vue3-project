7e877328c2a37f1d885e21290eeb3bdc7cbdd08b
ahmed <ahmed@DESKTOP-PUH2MTU>
2023-06-25 16:09:27 +0300

    add project

---
+++ vue-app/src/components/TheWelcome.vue
@@ -0,0 +1,35 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>who am i?</template>
+    I am FullStack developer with laravel and new for vue js, I hope to develop
+    modern website with new technologies.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Tooling</template>
+    Html, Css, and JavaScript also modern framework like laravel and vue.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>connect me</template>
+    <a href="mailto:a.3.shater@gmail.com">here.</a>
+  </WelcomeItem>
+</template>
